376def0be789dfa4fe80bbbcdc58a144be79c4e9
brendan.busch@uwa.edu.au <brendan.busch@uwa.edu.au>
2020-02-07 14:05:12 +0800

    Added model

---
+++ models/HNupgrade_model/HN/input/HNupgrade_Calibration_2011_v2/run_HNupgrade_Calibration_2011_v2.bat
@@ -0,0 +1,5 @@
+set OMP_NUM_THREADS = 16
+"D:\TUFLOWFV\FV2019\x64\TUFLOWFV.exe" "D:\TUFLOWFV\HN\input\HNupgrade_Calibration_2011_v2\HNupgrade_Calibration_warmup.fvc"
+"D:\TUFLOWFV\FV2019\x64\TUFLOWFV.exe" "D:\TUFLOWFV\HN\input\HNupgrade_Calibration_2011_v2\HNupgrade_Calibration_2005.fvc"
+"D:\TUFLOWFV\FV2019\x64\TUFLOWFV.exe" "D:\TUFLOWFV\HN\input\HNupgrade_Calibration_2011_v2\HNupgrade_Calibration_2006.fvc"
+cmd.exe

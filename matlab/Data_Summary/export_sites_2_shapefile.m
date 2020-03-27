c857e5fd8c3a938fde333d076b7369ea42935837
brendan-busch <brendan.busch@uwa.edu.au>
2020-03-27 13:25:34 +0800

    Field data sites shapefile

---
+++ matlab/Data_Summary/export_sites_2_shapefile.m
@@ -0,0 +1,19 @@
+clear all; close all;
+
+load ../Data_Import_2017/hawkesbury_all.mat;
+
+
+sites = fieldnames(hawkesbury_all);
+
+for i = 1:length(sites)
+    vars = fieldnames(hawkesbury_all.(sites{i}));
+    
+    S(i).X = hawkesbury_all.(sites{i}).(vars{1}).X;
+    S(i).Y = hawkesbury_all.(sites{i}).(vars{1}).Y;
+    S(i).Names = sites{i};
+    S(i).Agency = hawkesbury_all.(sites{i}).(vars{1}).Agency;
+    S(i).Geometry = 'Point';
+    
+end
+
+shapewrite(S,'Field_Data_sites.shp');

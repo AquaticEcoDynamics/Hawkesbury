57365f1ac376febc44bd83630e76b97dbcb0689c
brendan-busch <brendan.busch@uwa.edu.au>
2021-09-15 13:21:33 +0800

    Data Summary and GIS files

---
+++ matlab/Finalise_data/summary/run_summaries.m
@@ -0,0 +1,113 @@
+% 
+% 
+datafile = '..\hawkesbury_all.mat';
+
+summary_folder = 'hawkesbury_all\';
+
+gis_folder = 'gis\';
+
+shpname = 'hawkesbury_all.shp';
+
+summerise_data(datafile,summary_folder,gis_folder,shpname)
+% 
+% %_________________________________________________________________________
+% 
+% datafile = '..\..\..\data\store\hydro\UA_temperature_loggers.mat';
+% 
+% summary_folder = '..\..\..\data\summary\UA_temperature_loggers\';
+% 
+% gis_folder = '..\..\..\gis\summary\';
+% 
+% shpname = 'UA_temperature_loggers.shp';
+% 
+% summerise_data(datafile,summary_folder,gis_folder,shpname);
+% 
+% %_________________________________________________________________________
+% 
+% datafile = '..\..\..\data\store\hydro\dew_barrage.mat';
+% 
+% summary_folder = '..\..\..\data\summary\dew_barrage\';
+% 
+% gis_folder = '..\..\..\gis\summary\';
+% 
+% shpname = 'dew_barrage.shp';
+% 
+% summerise_data(datafile,summary_folder,gis_folder,shpname)
+% 
+% %_________________________________________________________________________
+% 
+% datafile = '..\..\..\data\store\metocean\NRM_metdata.mat';
+% 
+% summary_folder = '..\..\..\data\summary\NRM_metdata\';
+% 
+% gis_folder = '..\..\..\gis\summary\';
+% 
+% shpname = 'NRM_metdata.shp';
+% 
+% summerise_data(datafile,summary_folder,gis_folder,shpname)
+% 
+% %_________________________________________________________________________
+% %
+% 
+% datafile = '..\..\..\data\store\hydro\dew_tide_VH.mat';
+% 
+% summary_folder = '..\..\..\data\summary\dew_tide_VH\';
+% 
+% gis_folder = '..\..\..\gis\summary\';
+% 
+% shpname = 'dew_tide_VH.shp';
+% 
+% summerise_data(datafile,summary_folder,gis_folder,shpname)
+% 
+% %_________________________________________________________________________
+% 
+% 
+% datafile = '..\..\..\data\store\hydro\dew_WaterDataSA_hourly.mat';
+% 
+% summary_folder = '..\..\..\data\summary\dew_WaterDataSA_hourly\';
+% 
+% gis_folder = '..\..\..\gis\summary\';
+% 
+% shpname = 'dew_WaterDataSA.shp';
+% 
+% summerise_data(datafile,summary_folder,gis_folder,shpname)
+% 
+% %_________________________________________________________________________
+% 
+% 
+% datafile = '..\..\..\data\store\metocean\wave_wind.mat';
+% 
+% summary_folder = '..\..\..\data\summary\UA_wave_wind\';
+% 
+% gis_folder = '..\..\..\gis\summary\';
+% 
+% shpname = 'UA_wave_wind.shp';
+% 
+% summerise_data(datafile,summary_folder,gis_folder,shpname)
+
+
+%_________________________________________________________________________
+
+
+% datafile = '..\..\..\data\store\ecology\ALS.mat';
+% 
+% summary_folder = '..\..\..\data\summary\ALS\';
+% 
+% gis_folder = '..\..\..\gis\summary\';
+% 
+% shpname = 'ALS.shp';
+% 
+% summerise_data(datafile,summary_folder,gis_folder,shpname)
+
+%_________________________________________________________________________
+
+
+% datafile = '..\..\..\data\store\ecology\AWQC.mat';
+% 
+% summary_folder = '..\..\..\data\summary\AWQC\';
+% 
+% gis_folder = '..\..\..\gis\summary\';
+% 
+% shpname = 'AWQC.shp';
+% 
+% summerise_data(datafile,summary_folder,gis_folder,shpname)
